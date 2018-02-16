348eeb5c1665d7608a6c8fb23ccbff71b738d77b
Nathan Bittner <bittn037@umn.edu>
2018-02-15 23:27:54 -0600

    adding do_rollout function (from gym.examples)

--- utils.py
+++ utils.py
@@ -5,7 +5,19 @@ import numpy
 import gym
 ALL_ENVS = list(map(str, list(gym.envs.registry.all())))
 
-def env_search(query, n_return=10, _print=False):
+def do_rollout(agent, env, num_steps, render=False, render_step=2):
+    total_reward = 0
+    initial_observation = env.reset()
+    obs = initial_observation
+    for t in range(num_steps):
+        agent_action = agent.act(obs)
+        ob, reward, done, _info = env.step(agent_action)
+        total_reward += reward
+        if render and t%render_step==0: env.render()
+        if done: break
+    return total_reward, t+1
+
+def env_search(query, n_return=5, _print=False):
     query_results = [x[0] for x in
          sorted(list(map(lambda x: (x, SequenceMatcher(None, query, x).ratio()), ALL_ENVS)), key=lambda x: x[1],
                 reverse=True)][:n_return]
