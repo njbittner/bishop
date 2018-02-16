c2c532ad05255b107efa1c2f57f6aa1531f63dbd
Nathan Bittner <bittn037@umn.edu>
2018-02-15 23:16:21 -0600

    Made env_search util sort all registered envs by similarity ratio to querry string and return top n matches

--- utils.py
+++ utils.py
@@ -1,7 +1,15 @@
 from difflib import SequenceMatcher
+import pprint
+pp = pprint.pprint
 import numpy
 import gym
 ALL_ENVS = list(map(str, list(gym.envs.registry.all())))
 
-def env_search(query):
-    return [x for x in ALL_ENVS if SequenceMatcher(None, query, x).ratio() > 0.2]
+def env_search(query, n_return=10, _print=False):
+    query_results = [x[0] for x in
+         sorted(list(map(lambda x: (x, SequenceMatcher(None, query, x).ratio()), ALL_ENVS)), key=lambda x: x[1],
+                reverse=True)][:n_return]
+    if _print:
+        pp(query_results)
+
+    return query_results
